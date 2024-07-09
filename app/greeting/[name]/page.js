16b5e59e4c248eb7bfb2c09d689595a15241a7db
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-07-09 20:19:25 +0545

    navbar added and form made

--- app/greeting/[name]/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const page = ({params}) => {
-  return (
-    <div>page {params.name}</div>
-  )
-}
-
-export default page
--- app/page.js
+++ app/page.js
@@ -2,6 +2,12 @@
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full" />
+        <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
+        <button type="submit" className="bg-orange-600 py-3 px-11 text-white">Add ToDo</button>
+      </form>
+    </>
   );
 }
