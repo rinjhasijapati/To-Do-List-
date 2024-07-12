49c53c696e196320e856a2110382fa373db506ee
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-07-12 20:49:01 +0545

    dynamic todo component

--- Components/Todo.jsx
+++ Components/Todo.jsx
@@ -15,8 +15,9 @@ const Todo = () => {
             <td className="px-6 py-4">
                 Pending
             </td>
-            <td className="px-6 py-4">
+            <td className="px-6 py-4 flex gap-1">
                 <button className='py-2 px-4 bg-red-500 text-white'>Delete</button>
+                <button className='py-2 px-4 bg-green-500 text-white'>Done</button>
             </td>
         </tr>
     )
--- app/page.js
+++ app/page.js
@@ -1,12 +1,32 @@
+"use client"
+
 import Todo from "@/Components/Todo";
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData(form => ({...form,[name]:value}));
+    console.log(formData);
+  }
+
+  const onSubmitHandler = async () => {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
-        <input type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full" />
-        <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
+      <form onSubmit={onSubmitHandler} className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input value={formData.title} onChange={onChangeHandler} type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full" />
+        <textarea value={formData.description} onChange={onChangeHandler} name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
         <button type="submit" className="bg-orange-600 py-3 px-11 text-white">Add ToDo</button>
       </form>
 
@@ -34,6 +54,8 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
+            <Todo />
+            <Todo />
             <Todo />
           </tbody>
         </table>
